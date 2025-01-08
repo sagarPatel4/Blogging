94f9f9d6dbcceaf060dc14d73322e6c572ecfdee
Sagar Patel <sagarpatel04sr@gmail.com>
2025-01-09 02:15:31 +0530

    create base userl and home ,signin ,signup screen page

---
+++ index.js
@@ -0,0 +1,20 @@
+const express = require("express")
+const path =require("path")
+
+const {userRouter}=require("./routes/user")
+
+const app= express();
+const PORT =8000;
+
+app.set('view engine' , 'ejs')
+app.set('views', path.resolve("./views"))
+
+
+app.get('/',(req,res)=>{
+     res.render('home')
+})
+
+app.use("/user",userRouter)
+
+
+app.listen(PORT,()=>console.log(`Server started at PORT:${PORT} `))
---
+++ routes/user.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const router = express.Router();
+const User=require("../models/user");
+
+
+router.get('/signin', (req, res) => {
+    return res.render("signin");
+})
+router.get('/signup', (req, res) => {
+    return res.render("signup");
+})
+
+router.post('/signup', async(req, res) => {
+    const { fullName, email, password } = req.body;
+    await User.create({
+        fullName,
+        email,
+        password,
+    });
+    return res.redirect("/")
+})
+
+module.exports ={ router};
