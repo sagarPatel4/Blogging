efe5cc6e64fe9dbdd33d86c29ab1f592e09f8f3e
Sagar Radadiya <sagar.mivaninfotech@gmail.com>
2025-01-17 13:03:34 +0530

    add cooment funcationality in blog

--- app.js
+++ app.js
@@ -1,39 +1,42 @@
+require("dotenv").config();
+
 const express = require("express")
 const path = require("path")
 const mongoose = require("mongoose")
-const cookieParser=require("cookie-parser")
+const cookieParser = require("cookie-parser")
 
 const Blog = require('./models/blog')
 const userRouter = require("./routes/user")
 const blogRouter = require("./routes/blog")
 
-const { 
-     checkForAuthenticationCookie 
+const {
+     checkForAuthenticationCookie
 } = require("./middlewares/authentication")
 
-const app = express();
-app.use(express.static(path.resolve('./public')))
 
-const PORT = 8000;
+
+const app = express();
+const PORT = process.env.PORT || 8000;
 
 mongoose
-     .connect('mongodb://localhost:27017/blogify')
+     .connect(process.env.MONGO_URL)
      .then((e) => console.log("MongoDB Connected"));
 
 app.set('view engine', 'ejs')
 app.set('views', path.resolve("./views"));
 
+app.use(express.static(path.resolve('./public')))
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser())
 app.use(checkForAuthenticationCookie("token"))
 
 
-app.get('/', async(req, res) => {
-     const allBlogs =await Blog.find({});
-     res.render('home',{
-          user:req.user,
-          blog:allBlogs
+app.get('/', async (req, res) => {
+     const allBlogs = await Blog.find({});
+     res.render('home', {
+          user: req.user,
+          blog: allBlogs
      });
 });
 
